8e345313b750cf4390db25e514ec52a6879577f1
cmhnk <corinne@automatedinsights.com>
2016-11-24 12:55:32 -0500

    update belongs_to, has_many

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,21 @@
 class User < ApplicationRecord
+  as_many :microposts
 end
+
+
+# write out the analogous steps for visiting
+# the URL /users/1/edit
+
+# 1. broswer issues request for the /users/1/edit URL
+# 2. rails routes /users/1/edit to retrieve the information
+# associated with user with id = 1
+# 3. then does a GET request to edit the page and calls the
+# update method to PATCH the id = 1 record
+
+# this is the method that find user id = 1
+#
+# def set_user
+#  @user = User.find(params[:id])
+# end
+
+# edit.html.erb is the name of the view file for the user edit page
